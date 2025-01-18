96c4ea4f5b70ff4f48db1be1d74242c7b6712f67
Mbuldo <m.buldo@alustudent.com>
2025-01-18 06:51:03 +0000

    commit

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" LIFOCache module
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+   """ LIFOCache defines:
+     - caching system with LIFO algorithm
+     - max number of items from parent BaseCaching
+   """
+
+   def __init__(self):
+       """ Initialize cache
+           Call parent class constructor
+       """
+       super().__init__()
+       self.stack = []
+
+   def put(self, key, item):
+       """ Add an item in the cache using LIFO algorithm
+       Args:
+           key: key to add to cache
+           item: value to associate with key
+       """
+       if key is not None and item is not None:
+           if (len(self.cache_data) >= self.MAX_ITEMS and
+                   key not in self.cache_data):
+               last_key = self.stack.pop()
+               del self.cache_data[last_key]
+               print(f"DISCARD: {last_key}")
+
+           self.cache_data[key] = item
+           if key in self.stack:
+               self.stack.remove(key)
+           self.stack.append(key)
+
+   def get(self, key):
+       """ Get an item by key
+       Args:
+           key: key to look for in cache
+       Returns:
+           value associated with key if found, None otherwise
+       """
+       if key is not None:
+           return self.cache_data.get(key)
+       return None
